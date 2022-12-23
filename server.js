45a5c3764f0c17e0dbb903489755273b39b6acdb
Robert Johnson <robljohn1998@gmail.com>
2022-12-23 16:39:49 -0500

    adds functioning DB config and connection testing

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ require('dotenv').config()
 const session = require('express-session')
 const methodOverride = require('method-override')
 
+//DB Config
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//DB Connection
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + 'error running mongo'))
+db.on('connected', () => console.log('mongo is connected'))
+db.on('disconnected', () => console.log('disconnected from mongo'))
+
 //Listener
 const PORT = process.env.PORT
 app.listen(PORT, () => {
