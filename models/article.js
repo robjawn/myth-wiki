1e5623bc116bf1584db896d7bd1f230bbb15f69b
Robert Johnson <robljohn1998@gmail.com>
2023-01-02 21:41:05 -0500

    wip greek.js and adds article model schema

---
+++ models/article.js
@@ -0,0 +1,21 @@
+//Dependencies
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//article Schema
+const articleSchema = new Schema({
+    name: String,
+    type: String,
+    img: 
+    {
+        data: Buffer,
+        contentType: String,
+    },
+    description: String,
+})
+
+//Model
+const Article = mongoose.model('Article', articleSchema)
+
+//Export
+module.exports = Article
--- models/greek.js
+++ models/greek.js
@@ -3,84 +3,84 @@ module.exports = [
     id: "001",
     name: "Zeus",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Jupiter_J1a.jpg/800px-Jupiter_J1a.jpg",
     description: "God of the sky and thunder, Zeus is the ruler and father of all Olympians and humanity.",
 },
 {
     id: "002",
     name: "Demeter",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Demeter_Altemps_Inv8546.jpg/800px-Demeter_Altemps_Inv8546.jpg",
     description: "Goddess of the harvest and agriculture."
 },
 {
     id: "003",
     name: "Poseidon",
     type: "Olympians",
-    img: "",
+    img: "https://cdn.britannica.com/23/39023-050-B9E35664/Poseidon-marble-statue-Melos-National-Archaeological-Museum.jpg",
     description: `God of the sea, storms, earthquakes, and horses, Poseidon is known by the title "earth shaker".`,
 },
 {
     id: "004",
     name: "Hades",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Detail_of_Pluto-Serapis%2C_Statue_group_of_Persephone_%28as_Isis%29_and_Pluto_%28as_Serapis%29%2C_from_the_Sanctuary_of_the_Egyptian_Gods_at_Gortyna%2C_mid-2nd_century_AD%2C_Heraklion_Archaeological_Museum_%2830305313721%29.jpg/800px-thumbnail.jpg",
     description: "God of the dead and king of the underworld."
 },
 {
     id: "005",
     name: "Hera",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Divinit%C3%A0_sul_tipo_della_hera_borghese%2C_copia_romana_da_originale_della_scuola_di_fidia%2C_da_tor_bovacciana_%28ostia%29%2C_inv._2246.JPG/800px-Divinit%C3%A0_sul_tipo_della_hera_borghese%2C_copia_romana_da_originale_della_scuola_di_fidia%2C_da_tor_bovacciana_%28ostia%29%2C_inv._2246.JPG",
     description: "Goddess of marriage, women, and family, and the protector of women during childbirth. Wife (and sister) of Zeus."
 },
 {
     id: "006",
     name: "Apollo",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Italy-3104_-_Apollo_%285378415112%29.jpg/800px-Italy-3104_-_Apollo_%285378415112%29.jpg",
     description: "God of the Sun, archery, music, poetry, healing, and prophecy."
 },
 {
     id: "007",
     name: "Artemis",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Diana_of_Versailles.jpg/800px-Diana_of_Versailles.jpg",
     description: "Goddess of hunting and nature."
 },
 {
     id: "008",
     name: "Aphrodite",
     type: "Olympians",
-    img: "",
+    img: "https://cdn.britannica.com/62/181762-050-5DC9A4C7/Venus-Genetrix-Roman-marble-copy-statue-Callimachus-c-475-bce.jpg",
     description: "Goddess of beauty, passion, and love."
 },
 {
     id: "009",
     name: "Ares",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Ares_Ludovisi_Altemps_Inv8602_n2.jpg/800px-Ares_Ludovisi_Altemps_Inv8602_n2.jpg",
     description: "God of war and courage. Represents the brutality and bloodlust of war."
 },
 {
     id: "010",
     name: "Athena",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Mattei_Athena_Louvre_Ma530_n2.jpg/800px-Mattei_Athena_Louvre_Ma530_n2.jpg",
     description: "Goddess of wisdom and war. Represents the strategy and generalship of war."
 },
 {
     id: "011",
     name: "Hephaestus",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vulcan_Coustou_Louvre_MR1814.jpg/800px-Vulcan_Coustou_Louvre_MR1814.jpg",
     description: "God of fire and blacksmithing."
 },
 {
     id: "012",
     name: "Hermes",
     type: "Olympians",
-    img: "",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Hermes_Ingenui_Pio-Clementino_Inv544.jpg/800px-Hermes_Ingenui_Pio-Clementino_Inv544.jpg",
     description: "God of wealth, trade, thieves, and travelers."
 },
 ]
